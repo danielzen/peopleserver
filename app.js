ee77b654a892ee076ebde582bf45666b0e9b98d8
Daniel Zen <daniel@zen.digital>
2016-06-15 16:52:43 -0400

    .gitignore log & only run on port 8080

--- app.js
+++ app.js
@@ -152,9 +152,6 @@ function abort(err) {
 
 initialize().then(runApplication, abort);
 
-app.set('port', process.env.PORT || 8080);
-var server = app.listen(app.get('port'), function () {
-  console.log('Listening on port ' + server.address().port);
-});
+process.env.PORT = 8080;
 
 module.exports = app;
